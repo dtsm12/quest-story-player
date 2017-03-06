753ba51cc0fb6016ba6032cac8257b309df780ff
maitlada <David.TS.Maitland@gmail.com>
2017-03-06 23:25:06 +0000

    Enhanced integration test for all attribute types

--- src/test/java/net/maitland/quest/model/QuestMLTest.java
+++ src/test/java/net/maitland/quest/model/QuestMLTest.java
@@ -23,13 +23,25 @@ public class QuestMLTest {
             Quest sut = getQuest();
             Game game = sut.newGameInstance();
 
-            // enter quest
+            // start quest
             GameStation station = sut.getNextStation(game, 0);
+
+            // move to next station
+            station = sut.getNextStation(game, 2);
             List<GameChoice> choices = station.getChoices();
 
             while ("end".equals(station.getId()) == false){
 
-                assertEquals(station.getId() + " does not have 2 choices.", 2, choices.size());
+                String stationId = station.getId();
+                String attributeValue = game.getAttributeValue(stationId);
+
+                assertEquals(stationId + " does not have 2 choices.", 2, choices.size());
+                assertEquals(stationId + " has the wrong text.", "test "+attributeValue, station.getText());
+
+                GameChoice choice1 = choices.get(0);
+                assertEquals(stationId + " choice 1 has the wrong text.", "test "+attributeValue, choice1.getText());
+                assertEquals(stationId + " choice 1 has the wrong stationId.", "test"+attributeValue, choice1.getStationId());
+
                 station = sut.getNextStation(game, 2);
                 choices = station.getChoices();
             }
